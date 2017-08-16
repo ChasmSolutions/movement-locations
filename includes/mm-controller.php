eab168f142c18ff40a1e6726a8c9180bdf220819
Chris Wynn <chris@chasm.solutions>
2017-08-16 16:30:16 -0600

    Working geojson rest response

--- includes/mm-controller.php
+++ includes/mm-controller.php
@@ -35,105 +35,147 @@ class MM_Controller {
             ),
             ARRAY_A
         );
+    
+        $geojson = [
+            'type' => 'FeatureCollection',
+            'features'  => []
+        ];
         
         //prepare returns in foreach loop for geojson
-        
-        $geojson =
-            [
-                'type' => 'FeatureCollection',
-                'features'  => [
-                    [
-                        'type' => 'Feature',
-                        'geometry' => [
-                            'type' => 'Polygon',
-                            'coordinates' => [
-                                [
-                                    [100.0, 0.0],
-                                    [101.0, 0.0],
-                                    [101.0, 1.0],
-                                    [100.0, 1.0],
-                                    [100.0, 0.0]
-                                ]
-                            ]
-                        ],
-                        'properties' => [
-                            'WorldID' => (string) $CntyID,
-                            'Zone_Name' => '0',
-                            'CntyID' => '0',
-                            'Cnty_Name' => '0',
-                            'Adm1ID' => '0',
-                            'Adm1_Name' => '0',
-                            'Adm2ID' => '0',
-                            'Adm2_Name' => '0',
-                            'Adm3ID' => '0',
-                            'Adm3_Name' => '0',
-                            'Adm4ID' => '0',
-                            'Adm4_Name' => '0',
-                            'World' => '0',
-                            'Population' => '0',
-                            'Shape_Leng' => '0',
-                            'Cen_x' => '0',
-                            'Cen_y' => '0',
-                            'Region' => '0',
-                            'Field' => '0',
-                            'geometry' => '0',
-                            'OBJECTID_1' => '0',
-                            'OBJECTID' => '0',
-                            'Notes' => '0',
-                            'Last_Sync' => '0',
-                            'Sync_Source' => '0'
-                        ]
+        foreach ( $data as $record ) {
+            $geojson['features'][] =
+                [
+                    'type' => 'Feature',
+                    'geometry' => [
+                        'type' => 'Polygon',
+                        'coordinates' =>
+                            json_decode($record['geometry'])
+                        
                     ],
-                    [
-                        'type' => 'Feature',
-                        'geometry' => [
-                            'type' => 'Polygon',
-                            'coordinates' => [
-                                [
-                                    [100.0, 0.0],
-                                    [101.0, 0.0],
-                                    [101.0, 1.0],
-                                    [100.0, 1.0],
-                                    [100.0, 0.0]
-                                ]
-                            ]
-                        ],
-                        'properties' => [
-                            'WorldID' => (string) $CntyID,
-                            'Zone_Name' => '0',
-                            'CntyID' => '0',
-                            'Cnty_Name' => '0',
-                            'Adm1ID' => '0',
-                            'Adm1_Name' => '0',
-                            'Adm2ID' => '0',
-                            'Adm2_Name' => '0',
-                            'Adm3ID' => '0',
-                            'Adm3_Name' => '0',
-                            'Adm4ID' => '0',
-                            'Adm4_Name' => '0',
-                            'World' => '0',
-                            'Population' => '0',
-                            'Shape_Leng' => '0',
-                            'Cen_x' => '0',
-                            'Cen_y' => '0',
-                            'Region' => '0',
-                            'Field' => '0',
-                            'geometry' => '0',
-                            'OBJECTID_1' => '0',
-                            'OBJECTID' => '0',
-                            'Notes' => '0',
-                            'Last_Sync' => '0',
-                            'Sync_Source' => '0'
-                        ]
+                    'properties' => [
+                        'WorldID' => $record['WorldID'],
+                        'Zone_Name' => $record['Zone_Name'],
+                        'CntyID' => $record['CntyID'],
+                        'Cnty_Name' => $record['Cnty_Name'],
+                        'Adm1ID' => $record['Adm1ID'],
+                        'Adm1_Name' => $record['Adm1_Name'],
+                        'Adm2ID' => $record['Adm2ID'],
+                        'Adm2_Name' => $record['Adm2_Name'],
+                        'Adm3ID' => $record['Adm3ID'],
+                        'Adm3_Name' => $record['Adm3_Name'],
+                        'Adm4ID' => $record['Adm4ID'],
+                        'Adm4_Name' => $record['Adm4_Name'],
+                        'World' => $record['World'],
+                        'Population' => $record['Population'],
+                        'Shape_Leng' => $record['Shape_Leng'],
+                        'Cen_x' => $record['Cen_x'],
+                        'Cen_y' => $record['Cen_y'],
+                        'Region' => $record['Region'],
+                        'Field' => $record['Field'],
+                        'OBJECTID_1' => $record['OBJECTID_1'],
+                        'Notes' => $record['Notes'],
+                        'Last_Sync' => $record['Last_Sync'],
+                        'Sync_Source' => $record['Sync_Source']
                     ]
-                    
-                ]
-                
-            ];
+                ];
+        }
+        
+//        $geojson =
+//            [
+//                'type' => 'FeatureCollection',
+//                'features'  => [
+//                    [
+//                        'type' => 'Feature',
+//                        'geometry' => [
+//                            'type' => 'Polygon',
+//                            'coordinates' => [
+//                                [
+//                                    [100.0, 0.0],
+//                                    [101.0, 0.0],
+//                                    [101.0, 1.0],
+//                                    [100.0, 1.0],
+//                                    [100.0, 0.0]
+//                                ]
+//                            ]
+//                        ],
+//                        'properties' => [
+//                            'WorldID' => (string) $CntyID,
+//                            'Zone_Name' => '0',
+//                            'CntyID' => '0',
+//                            'Cnty_Name' => '0',
+//                            'Adm1ID' => '0',
+//                            'Adm1_Name' => '0',
+//                            'Adm2ID' => '0',
+//                            'Adm2_Name' => '0',
+//                            'Adm3ID' => '0',
+//                            'Adm3_Name' => '0',
+//                            'Adm4ID' => '0',
+//                            'Adm4_Name' => '0',
+//                            'World' => '0',
+//                            'Population' => '0',
+//                            'Shape_Leng' => '0',
+//                            'Cen_x' => '0',
+//                            'Cen_y' => '0',
+//                            'Region' => '0',
+//                            'Field' => '0',
+//                            'geometry' => '0',
+//                            'OBJECTID_1' => '0',
+//                            'OBJECTID' => '0',
+//                            'Notes' => '0',
+//                            'Last_Sync' => '0',
+//                            'Sync_Source' => '0'
+//                        ]
+//                    ],
+//                    [
+//                        'type' => 'Feature',
+//                        'geometry' => [
+//                            'type' => 'Polygon',
+//                            'coordinates' => [
+//                                [
+//                                    [100.0, 0.0],
+//                                    [101.0, 0.0],
+//                                    [101.0, 1.0],
+//                                    [100.0, 1.0],
+//                                    [100.0, 0.0]
+//                                ]
+//                            ]
+//                        ],
+//                        'properties' => [
+//                            'WorldID' => (string) $CntyID,
+//                            'Zone_Name' => '0',
+//                            'CntyID' => '0',
+//                            'Cnty_Name' => '0',
+//                            'Adm1ID' => '0',
+//                            'Adm1_Name' => '0',
+//                            'Adm2ID' => '0',
+//                            'Adm2_Name' => '0',
+//                            'Adm3ID' => '0',
+//                            'Adm3_Name' => '0',
+//                            'Adm4ID' => '0',
+//                            'Adm4_Name' => '0',
+//                            'World' => '0',
+//                            'Population' => '0',
+//                            'Shape_Leng' => '0',
+//                            'Cen_x' => '0',
+//                            'Cen_y' => '0',
+//                            'Region' => '0',
+//                            'Field' => '0',
+//                            'geometry' => '0',
+//                            'OBJECTID_1' => '0',
+//                            'OBJECTID' => '0',
+//                            'Notes' => '0',
+//                            'Last_Sync' => '0',
+//                            'Sync_Source' => '0'
+//                        ]
+//                    ]
+//
+//                ]
+//
+//            ];
         
         return [
             'status' => 'OK',
-            'geojson' => $data,
+            'geojson' => $geojson,
         ];
     }
     
